35c2b1323ef988a1a511df81df61d6c5d3e4c383
Manny Cortez Studios <emailme@mannycortez.com>
2020-10-14 11:26:03 -0700

    Added Ratings feature and basic CSS to homepage

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
-
+import Rating from './Rating'
+ 
 const Product = ({ product }) => {
     return (
         <Card className = 'my-3 p-3 rounded'>
@@ -14,9 +15,10 @@ const Product = ({ product }) => {
             </a>
 
             <Card.Text as='div'>
-                <div className='my-3'>
-                    {product.rating} from {product.numReviews} reviews
-                </div>
+                <Rating 
+                value={product.rating} 
+                text={`${product.numReviews} reviews`}>
+                </Rating>
             </Card.Text>
 
     <Card.Text as='h3'>${product.price}</Card.Text>
