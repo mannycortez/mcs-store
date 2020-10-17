af409e94194d190db73b02a07179d17413a07fe7
Manny Cortez Studios <emailme@mannycortez.com>
2020-10-17 11:47:02 -0700

    Added Order Screen

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -60,7 +60,7 @@ export const getOrderDetails = (id) => async(dispatch, getState) => {
         }
 
         const { data } = await axios.get(
-            `/api/orders${id}`,
+            `/api/orders/${id}`,
             config
         )
         dispatch({
