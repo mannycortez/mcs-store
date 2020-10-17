9cf9b5a3b91284134609fecf6ad953eab227195b
Manny Cortez Studios <emailme@mannycortez.com>
2020-10-17 11:10:06 -0700

    Added Order Details reducer and action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -8,7 +8,7 @@ import { userLoginReducer,
          userDetailsReducer,
          userUpdateProfileReducer } from './reducers/userReducers'
 
-import { orderCreateReducer } from './reducers/orderReducers'
+import { orderCreateReducer, orderDetailsReducer } from './reducers/orderReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -18,7 +18,8 @@ const reducer = combineReducers({
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
-    orderCreate: orderCreateReducer 
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer 
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
