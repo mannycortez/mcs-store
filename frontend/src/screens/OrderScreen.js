e293f4fbe3c8407bd3422cd2081486202fec670c
Manny Cortez Studios <emailme@mannycortez.com>
2020-10-17 13:12:24 -0700

    Added OrderPay reducer and action

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -27,9 +27,8 @@ const OrderScreen = ({ match }) => {
     }
 
     useEffect(() => {
-        if(!order || order._id !== orderId) {
         dispatch(getOrderDetails(orderId))
-    }}, [order, orderId])
+    }, [dispatch, orderId])
 
     return loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : <>
         <h1>Order {order._id}</h1>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,9 +6,13 @@ import { cartReducer } from './reducers/cartReducers'
 import { userLoginReducer, 
          userRegisterReducer, 
          userDetailsReducer,
-         userUpdateProfileReducer } from './reducers/userReducers'
+         userUpdateProfileReducer
+ } from './reducers/userReducers'
 
-import { orderCreateReducer, orderDetailsReducer } from './reducers/orderReducers'
+import { 
+    orderCreateReducer, 
+    orderDetailsReducer,
+    orderPayReducer } from './reducers/orderReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -19,7 +23,8 @@ const reducer = combineReducers({
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
     orderCreate: orderCreateReducer,
-    orderDetails: orderDetailsReducer 
+    orderDetails: orderDetailsReducer,
+    orderPay: orderPayReducer 
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
