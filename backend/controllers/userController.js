65af9d95580d3e5cd6b7658140d10ae49b686a1f
Manny Cortez Studios <emailme@mannycortez.com>
2020-10-17 22:38:17 -0700

    Added Admin middleware and getUsers Endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -106,4 +106,25 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
-export { authUser, registerUser, getUserProfile, updateUserProfile }
+//@desc Get all users
+//@route GET /api/users
+//@access Private/Admin
+
+const getUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({})
+    res.json(users)
+
+    if(user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -25,7 +25,7 @@ const Header = () => {
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="ml-auto">
       <LinkContainer to='/cart'>
-        <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+        <Nav.Link><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
       </LinkContainer>
       {userInfo ? (
         <NavDropdown title={userInfo.name} id='username'>
@@ -37,7 +37,7 @@ const Header = () => {
           </NavDropdown.Item>
         </NavDropdown>
       ) : <LinkContainer to='/login'>
-            <Nav.Link><i className='fas fa-user'></i>Sign In</Nav.Link>
+            <Nav.Link><i className='fas fa-user'></i> Sign In</Nav.Link>
           </LinkContainer> }
     </Nav>
   </Navbar.Collapse>
