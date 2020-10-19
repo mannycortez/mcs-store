b57dac7d169d7ed61d84d497686ab48ddc8cc584
Manny Cortez Studios <emailme@mannycortez.com>
2020-10-18 23:50:45 -0700

    Finished Admin Order List

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -24,7 +24,8 @@ import {
     orderCreateReducer, 
     orderDetailsReducer,
     orderPayReducer,
-    orderListMyReducer } from './reducers/orderReducers'
+    orderListMyReducer,
+    orderListReducer } from './reducers/orderReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -43,7 +44,8 @@ const reducer = combineReducers({
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
     orderPay: orderPayReducer,
-    orderListMy: orderListMyReducer 
+    orderListMy: orderListMyReducer,
+    orderList: orderListReducer 
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
